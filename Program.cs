c690802262b68867b3e4f83f5dfeabf0d6a6b823
Emanuel Edvan <105885812+RedLan2@users.noreply.github.com>
2024-05-11 22:40:06 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using DesafioPOO.Models;
+
+// TODO: Realizar os testes com as classes Nokia e Iphone
+Iphone iphone=new Iphone("3333","pro","1111",5);
+iphone.Ligar();
+Console.WriteLine($"O {iphone.Modelo} tem {iphone.Memoria}GB de memoria");
+iphone.InstalarAplicativo("Zap");
+Nokia  nokia=new Nokia("3333","win","1111",32);
+nokia.ReceberLigacao();
+Console.WriteLine($"O {nokia.Modelo} tem {nokia.Memoria}GB de memoria");
+
+
+
